feceb14b03be79f6c501e41e44784d4e9f2a7c2b
Karan Sakhuja <karan.sakhuja@outlook.com>
2024-03-14 10:57:05 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true
+});
+
+fastify.get('/', function (request, reply) {
+  reply.send(process.env);
+});
+
+fastify.listen({ port: process.env.PORT }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`Server is now listening on ${address}`);
+});
